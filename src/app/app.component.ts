cb8a49325a5fc4260c8f14903604958515e28978
Roberto Carlos Apaza Cornejo <r.apaza.cornejo@gmail.com>
2025-05-29 00:23:16 -0500

    Refactor app structure: remove unused components and styles, add routing for product pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,11 @@
 import { Component } from '@angular/core';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
-import { ToastModule } from 'primeng/toast';
-import { ButtonModule } from 'primeng/button';
-import { MessageService } from 'primeng/api';
-import { InputTextModule } from 'primeng/inputtext';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [ToastModule, ButtonModule, InputTextModule, ReactiveFormsModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
-  providers: [MessageService],
+  imports: [RouterOutlet],
+  template: '<router-outlet/>',
 })
 export class AppComponent {
   title = 'frontend';
-  value = new FormControl('');
-
-  constructor(private messageService: MessageService) {}
-
-  show() {
-    this.messageService.add({
-      severity: 'info',
-      summary: 'Info',
-      detail: 'Message Content',
-      life: 3000,
-    });
-  }
-
-  toggleDarkMode() {
-    const element = document.querySelector('html');
-    element?.classList.toggle('my-app-dark');
-  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './domains/products/pages/product-list/product-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+  },
+  {
+    path: 'products',
+    component: ProductListComponent,
+  },
+];
