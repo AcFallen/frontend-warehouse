95854b002d07c08d1826201a0362932211a0db6c
Roberto Carlos Apaza Cornejo <r.apaza.cornejo@gmail.com>
2025-05-28 23:57:11 -0500

    Add Tailwind CSS and PrimeNG integration with dark mode support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { ToastModule } from 'primeng/toast';
+import { ButtonModule } from 'primeng/button';
+import { MessageService } from 'primeng/api';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ToastModule, ButtonModule, InputTextModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers: [MessageService],
 })
 export class AppComponent {
   title = 'frontend';
+  value = new FormControl('');
+
+  constructor(private messageService: MessageService) {}
+
+  show() {
+    this.messageService.add({
+      severity: 'info',
+      summary: 'Info',
+      detail: 'Message Content',
+      life: 3000,
+    });
+  }
+
+  toggleDarkMode() {
+    const element = document.querySelector('html');
+    element?.classList.toggle('my-app-dark');
+  }
 }
